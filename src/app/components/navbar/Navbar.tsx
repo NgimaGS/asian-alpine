47b12a1edfb7a3c0b9ca646649e57f33b641320c
NgimaGS <ngimags19@gmail.com>
2023-11-05 14:56:35 +0545

    navbar update

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,5 +1,18 @@
 import { Outlet } from "react-router-dom";
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import useScrollTrigger from "@mui/material/useScrollTrigger";
 import * as React from "react";
 
@@ -28,41 +41,109 @@ function ElevationScroll(props: Props) {
   });
 }
 
+const drawerWidth = 240;
+const navItems = ["Home", "About Us", "Destination", "Activity", "Contact Us"];
+
 const Navbar = (props: Props) => {
+  const { window } = props;
+
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 700,
   });
 
   const backgroundColor = trigger ? "#000" : "transparent";
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
     <div>
       <ElevationScroll {...props}>
-        <AppBar sx={{ background: backgroundColor }}>
+        <AppBar sx={{ background: backgroundColor, pb: 1 }}>
           <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ pr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
             <img
               src="/assets/logo/logo.png"
               alt="logo"
               style={{ width: "120px" }}
             />
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-around",
-                maxWidth: "500px",
+            <Box
+              sx={{
+                display: { xs: "none", md: "flex" },
+                maxWidth: "700px",
                 width: "100%",
               }}
             >
-              <Typography component="div">Home</Typography>
-              <Typography component="div">About Us</Typography>
-              <Typography component="div">Destination</Typography>
-              <Typography component="div">Activity</Typography>
-              <Typography component="div">Contact Us</Typography>
-            </div>
+              <List
+                sx={{
+                  display: { xs: "none", md: "flex" },
+                  width: "100%",
+                }}
+              >
+                {navItems.map((item) => (
+                  <ListItem key={item} disablePadding>
+                    <ListItemButton sx={{ textAlign: "center" }}>
+                      <ListItemText primary={item} />
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+              </List>
+            </Box>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
       <Box>
         <Outlet />
       </Box>
